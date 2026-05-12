ff1838c05561b75a3194b788cdbdd62e7f4139cc
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-05-11 17:49:38 -0700

    Updated main.hpp

--- main.hpp
+++ main.hpp
@@ -16,16 +16,55 @@ void findMaxVal(int[], int[][NUM_COLS], int, int);
 void findMaxSum(int result[], int num[][NUM_COLS], int rows, int cols)
 {
     // TODO: find the row whose sum is the largest, copy that row into result.
+    int maxsum, maxrow;
+    for(int i = 0; i < NUM_ROWS; i++){
+        int total = 0;
+        for(int j = 0; j < NUM_COLS; j++){
+            total += num[i][j];
+            if((i == 0) || maxsum < total){
+                maxsum = total; maxrow = i;
+            }
+            
+        }
+    }
+    for(int j = 0; j < NUM_COLS; j++){
+        result[j] = num[maxrow][j];
+    }
 }
-
 void findMaxElm(int result[], int num[][NUM_COLS], int rows, int cols)
 {
     // TODO: for each row, store the maximum element of that row in result[i].
+    int maxelm, currval;
+    for(int i = 0; i < NUM_ROWS; i++){
+        maxelm = num[i][0];
+        for(int j = 0; j < NUM_COLS; j++){
+            currval = num[i][j];
+            if(maxelm < currval)
+                maxelm = currval;
+                result[i] = maxelm;
+        }
+    }
+
 }
 
 void findMaxVal(int result[], int num[][NUM_COLS], int rows, int cols)
 {
     // TODO: find the cell (r,c) holding the global maximum value; copy row r.
+    int maxval, maxrow, currval;
+    maxval = num[0][0];
+    for(int i = 0; i < NUM_ROWS; i++){
+        for(int j = 0; j < NUM_COLS; j++){
+            currval = num[i][j];
+            if((i == 0) || maxval < currval){
+                maxval = currval; maxrow = i;
+            }
+        }
+    }
+
+    for(int j = 0; j < NUM_COLS; j++){
+        result[j] = num[maxrow][j];
+    }
+
 }
 
 #endif
